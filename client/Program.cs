664f1dae0ba4a16a757ec4fe7169c64107628c4d
Galanty4 <61089538+Galanty4@users.noreply.github.com>
2023-04-17 20:50:45 +0200

    init

---
+++ client/Program.cs
@@ -0,0 +1,20 @@
+﻿using popup;
+using System;
+
+namespace client
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Client");
+
+            var wpfApp1 = new App();
+            wpfApp1.Run();
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ popup/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace popup
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // utworzenie okna głównego
+            var mainWindow = new MainWindow();
+
+            // wyświetlenie okna głównego za pomocą metody Show()
+            mainWindow.Show();
+        }
+    }
+}
